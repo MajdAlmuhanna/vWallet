57b872719070295ca7350177976dcb14c5084c60
MajdAlmuhanna <439201099@student.ksu.edu.sa>
2021-10-10 19:55:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vwallet/userInformation.java
@@ -0,0 +1,14 @@
+package com.example.vwallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_information);
+    }
+}
